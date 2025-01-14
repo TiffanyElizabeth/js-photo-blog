dac3d55e84c86361020b2308ae3ab9d0e9dd1227
tiffanybrezak <tiffany.elizabeth.b@gmail.com>
2025-01-14 14:15:27 +0100

    feat: add pointer cursor to posts

--- script.js
+++ script.js
@@ -20,8 +20,7 @@ PHOTOS NOT LOADING ****
 
 Bonus
 .. DONE rendi la pagina responsive, in modo che su mobile e tablet le foto si dispongano man mano una sotto l’altra ed il titolo abbia una dimensione adeguata 
-Note
-Non siete obbligati a usare Bootstrap: siete liberi di decidere come gestire lo stile 
+Note: Non siete obbligati a usare Bootstrap: siete liberi di decidere come gestire lo stile 
 */
 
 /* 
@@ -31,8 +30,8 @@ Milestone 1
 
 Milestone 2
 .. DONE Facciamo sparire l’overlay con l’aiuto di una classe CSS che imposti il display: none .
-.. Dopodiché facciamo sì che cliccando una qualunque foto. L’overlay ricompaia.
-.. Cliccando invece il button di chiusura, l’overlay scompare nuovamente.
+.. DONE Dopodiché facciamo sì che cliccando una qualunque foto (post). L’overlay ricompaia.
+.. DONE Cliccando invece il button di chiusura, l’overlay scompare nuovamente.
 
 Milestone 3
 .. Inseriamo il pezzo di logica finale: quando una foto viene cliccata, dobbiamo fare in modo che sia proprio quella foto a essere mostrata all’interno dell’overlay.
@@ -40,10 +39,9 @@ Ci sono diversi modi di farlo, prova a sperimentare
 
 Bonus
 .. Spostandosi col mouse sopra le foto, queste si zoommano, ruotano di 10 gradi e la loro ombra aumenta, il tutto in manierà fluida. 
-.. Inoltre il mouse diventa un puntatore, per far capire all’utente che può cliccare
+.. DONE Inoltre il mouse diventa un puntatore, per far capire all’utente che può cliccare
 */
 
-/* PRIMA CONSEGNA, Milestone 3 */
 
 const postContainerElm = document.getElementById("post-container");
 const postPhotoElm = document.getElementById("post-photo");
